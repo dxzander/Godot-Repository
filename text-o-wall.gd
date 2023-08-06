617d6e799ab8d34acb6ed1520f6844187b9b2bad
Alexis <42919405+dxzander@users.noreply.github.com>
2023-08-05 21:43:56 -0600

    some cleanup of the wall

---
+++ text-o-wall.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var color = 0.0
+@export_range(0, 15) var line_number: int
+
+var animation_start_position = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
