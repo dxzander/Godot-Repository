a84ed3a87d0788ad75636a3bde60fd6c12942968
Alexis <42919405+dxzander@users.noreply.github.com>
2023-08-06 21:51:27 -0600

    implementation of assignable text color for the wall

--- text-o-wall.gd
+++ text-o-wall.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
-@export var color = 0.0
+@export var color: Color
 @export_range(0, 15) var line_number: int
 
 var animation_start_position = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Label.label_settings.font_color = color
+	$AnimationPlayer.play("Text Slide")
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
