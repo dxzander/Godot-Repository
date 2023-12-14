2a86c7951eaaf32ee50004c2882afa5c1d847852
Alexis <42919405+dxzander@users.noreply.github.com>
2023-12-14 11:12:48 -0600

    blueprints update

--- blueprints.gd
+++ blueprints.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 var lines = preload("res://LineAnim.tscn")
+var counter = 0
+var counterAss = 0
+var thickLine = 6.0
+var thinLine = 2.0
+var curLine = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,10 +14,26 @@ func _ready():
 func _on_timer_timeout():
 	#vertical
 	var line_instance = lines.instantiate()
+	curLine = counter_mgmt()
+	if curLine == 0:
+		line_instance.lwidth = thickLine
+	else:
+		line_instance.lwidth = thinLine
 	add_child(line_instance)
 	#horizontal
 	line_instance = lines.instantiate()
 	line_instance.position.y = 1920
 	line_instance.rotation_degrees = -90
+	if curLine == 0:
+		line_instance.lwidth = thickLine
+	else:
+		line_instance.lwidth = thinLine
 	add_child(line_instance)
 	pass # Replace with function body.
+
+func counter_mgmt():
+	counterAss = counter
+	counter += 1
+	if counter > 4:
+		counter = 0
+	return counterAss
